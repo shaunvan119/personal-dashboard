9f023547a4ca89ea02ac478409bf0a5cdd2f36ac
shaunvan119 <shaunvan@live.com.au>
2022-04-30 18:28:53 +1000

    author unleash api

--- index.js
+++ index.js
@@ -4,8 +4,12 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
   .then(response => response.json())
   .then(data => {
-      console.log(data.urls.full)
-     document.body.style.backgroundImage = `url(${data.urls.full})`
+      console.log(data.user.first_name)
+     document.body.style.backgroundImage = `url(${data.urls.regular})`
+     document.getElementById("author").innerHTML = `
+     <p>${data.user.first_name}</p>
+
+     `
     });
 
 
