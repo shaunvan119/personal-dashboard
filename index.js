97d4e8b8f9daa511f6241739ef75cb16240c79ce
shaunvan119 <shaunvan@live.com.au>
2022-04-30 18:09:38 +1000

    refactored CSS and unleash API background image

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   .then(response => response.json())
   .then(data => {
       console.log(data.urls.full)
-     document.getElementById("background-image").style.background = `url(https://images.unsplash.com/photo-1500829243541-74b677fecc30?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTEzMDIwNTY&ixlib=rb-1.2.1&q=85)`
+     document.body.style.backgroundImage = `url(${data.urls.full})`
     });
 
 
