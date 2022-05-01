5c01e56f80692c1b3bb48675aee89899907a819b
shaunvan119 <shaunvan@live.com.au>
2022-05-01 17:32:12 +1000

    templet string by

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
       console.log(data.user.first_name)
      document.body.style.backgroundImage = `url(${data.urls.regular})`
      document.getElementById("author").innerHTML = 
-     document.getElementById("author").textContent = data.user.name
+     document.getElementById("author").textContent = `By: ${data.user.name}`
     
 
      
