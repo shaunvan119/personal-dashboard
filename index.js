941504b7aefe68a87cd67c3c0962ee52d5862782
shaunvan119 <shaunvan@live.com.au>
2022-05-01 17:24:35 +1000

    author first name and last name

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
       console.log(data.user.first_name)
      document.body.style.backgroundImage = `url(${data.urls.regular})`
      document.getElementById("author").innerHTML = `
-     <p>${data.user.first_name}</p>
+     <p>${data.user.first_name} ${data.user.last_name}</p>
+    
 
      `
     });
