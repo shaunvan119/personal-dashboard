facf8a8acced1391c6d634beb8239ea9748cab17
shaunvan119 <shaunvan@live.com.au>
2022-05-02 14:04:00 +1000

    weather data

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ setInterval(getCurrentTime, 1000)
 
 // weather
 navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
         .then(res => {
             if (!res.ok) {
                 throw Error("Weather data not available")
@@ -59,6 +59,12 @@ navigator.geolocation.getCurrentPosition(position => {
         })
         .then(data => {
             console.log(data)
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p>${Math.round(data.main.temp)}º</p>
+                <p>${data.name}</p>
+            `
         })
         .catch(err => console.error(err))
 });
